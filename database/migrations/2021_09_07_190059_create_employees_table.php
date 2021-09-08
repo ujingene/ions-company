e861ba5c2f0f558e30bff6b0d19b2577900b363c
Eugene Wanjira <ujingene@users.noreply.github.com>
2021-09-08 13:46:42 +0300

    Added Migration tables for the following
    - Companies
    - Employees
    - Todos
    
    Created a Seeder for Admin User

---
+++ database/migrations/2021_09_07_190059_create_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name')->nullable(false);
+            $table->string('last_name')->nullable(false);
+            $table->foreignId('company_id')->constrained();
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
